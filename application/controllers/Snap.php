89e8e436c85e47ed84c0b0a63598bab55fef8a6c
KevinAnantha <108852665+KevinAnantha@users.noreply.github.com>
2023-12-07 14:46:41 +0700

    Update Snap.php

--- application/controllers/Snap.php
+++ application/controllers/Snap.php
@@ -144,7 +144,7 @@ class Snap extends CI_Controller {
         $time = time();
         $custom_expiry = array(
             'start_time' => date("Y-m-d H:i:s O",$time),
-            'unit' => 'minute', 
+            'unit' => 'hours', 
             'duration'  => 1
         );
         
